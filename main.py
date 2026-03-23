a3e123fa844e81fc4eb3dd9ecba0f29f6fe48eec
Ramcharan Gubbala <ramcharangubbala7@gmail.com>
2026-03-23 13:33:29 -0500

    Data Ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-from mlproject.utils import logger
+from mlProject.utils import logger
+from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
+except Exception as e:
+         logger.exception(e)
+         raise e
 
 logger.info("Starting the ML project...")
---
+++ src/mlproject/constants/__init__.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH: Path = Path("config/config.yaml")
+PARAMS_FILE_PATH: Path = Path("params.yaml")
+SCHEMA_FILE_PATH: Path = Path("schema.yaml")
