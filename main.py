bda31332e89eb0db62f6830805ad438c548d5679
Ramcharan Gubbala <ramcharangubbala7@gmail.com>
2026-03-23 20:40:33 -0500

    Model Trainer added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 from mlProject.utils import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
@@ -31,6 +32,18 @@ STAGE_NAME = "Data Transformation Stage"
 try:
         logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
         obj = DataTransformationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
+except Exception as e:
+         logger.exception(e)
+         raise e
+
+
+STAGE_NAME = "Model Trainer Stage"
+
+try:
+        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+        obj = ModelTrainerTrainingPipeline()
         obj.main()
         logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
 except Exception as e:
--- src/mlproject/entity/config_entity.py
+++ src/mlproject/entity/config_entity.py
@@ -11,7 +11,7 @@ class DataIngestionConfig:
     
     
 
-@dataclass
+@dataclass(frozen=True)
 class DataValidationConfig:
     root_dir: Path
     unzip_data_dir: Path
@@ -21,8 +21,20 @@ class DataValidationConfig:
 from dataclasses import dataclass
 from pathlib import Path
 
-@dataclass
+@dataclass(frozen=True)
 class DataTransformationConfig:
     root_dir: Path
     data_path: Path
     
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass(frozen=True)
+class ModelTrainerConfig:
+    root_dir: Path
+    train_data_path: Path
+    test_data_path: Path
+    model_name: str
+    alpha: float
+    l1_ratio: float
+    target_column: str
