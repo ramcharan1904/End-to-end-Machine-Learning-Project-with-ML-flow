c493451660ec4bbdeb456c60c424b706f2b92064
Ramcharan Gubbala <ramcharangubbala7@gmail.com>
2026-04-02 20:13:32 -0500

    evaluation and UI added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+
+from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 from mlProject.utils import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
@@ -49,3 +51,14 @@ try:
 except Exception as e:
          logger.exception(e)
          raise e
+ 
+ 
+STAGE_NAME = "Model Evaluation Stage"
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelEvaluationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
